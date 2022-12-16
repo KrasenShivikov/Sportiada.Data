1c3b3195ef916c06a0f00b68f306e24c858c4f4c
KrasenShivikov <45423142+KrasenShivikov@users.noreply.github.com>
2022-12-16 10:22:39 +0200

    Delete Models directory

--- Models/User.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Sportiada.Data.Models
-{
-    using Microsoft.AspNetCore.Identity;
-
-    // Add profile data for application users by adding properties to the User class
-    public class User : IdentityUser
-    {
-    }
-}
